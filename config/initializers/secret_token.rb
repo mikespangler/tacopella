51d46368655b9eaf5e3b9b63165416a4e34b8958
tiffanypeon <tiffanypeon@gmail.com>
2013-12-02 15:23:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tacopella::Application.config.secret_key_base = '411b415f97b86754be0f3c812f3911e69e1ba866977ecd3f2b51cef7babd2667f81592eb54d11acb051ac6f4a64abeb30dd71b79d859484dce24e98400e993e9'
