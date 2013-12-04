2f5698c329d4f1917549995962403685cbc2a366
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-12-03 19:28:17 -0500

    rdio gem stuff

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,26 @@
 class SongsController < ApplicationController
+  before_action :require_login
+ 
+  def new
+    @api = api
+    @user = user
+  end
+
+  def create
+    api.search(params[:search_song])
+  end
+
+
+
+
+
+  private
+ 
+  def require_login
+    unless api
+      api = Rdio::Api.new('nwqk6482vnzn47ph332m5e6v', 'TxxUNrJC3v')
+    unless user
+      user = api.currentUser
+    end
+  end
 end
