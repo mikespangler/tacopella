0327abe2f1f9fe1eb68d285b404b1a2410c87957
danielchangNYC <daniel.chang85@gmail.com>
2013-12-05 01:49:40 -0500

    added invite friends and mailer links. styles on welcome, index, nav. JS for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,23 @@
 Tacopella::Application.routes.draw do
   resources :songs
-
   resources :challenges
-    get 'challenges/:id/invite-friends' => 'challenges#invite_friends_form', :as => :invite_friends
-    post 'challenges/:id/invite-friends' => 'challenges#invite_friends'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-   post 'challenges/add_songs' => 'challenges#add_songs', :as => :add_songs
-  # You can have the root of your site routed with "root"
-   root 'challenges#welcome'
+  root 'challenges#welcome'
+  get 'challenges/:id/invite-friends' => 'challenges#invite_friends_form', :as => :invite_friends
+  post 'challenges/invite-friends' => 'challenges#invite_friends'
 
-   get 'challenges/:id/songs/search' => 'songs#search_songs', :as => :search_songs
+  post 'challenges/add_songs' => 'challenges#add_songs', :as => :add_songs
 
-   #post 'challenges/:id/songs/search' => 'songs#create_search_results'
-   get 'challenges/:id/songs/song_results' => 'songs#song_results', :as => :song_results 
-   post 'songs/song_results' => 'challenges#update'
+  get 'challenges/:id/songs/search' => 'songs#search_songs', :as => :search_songs
+
+  #post 'challenges/:id/songs/search' => 'songs#create_search_results'
+  get 'challenges/:id/songs/song_results' => 'songs#song_results', :as => :song_results 
+  post 'songs/song_results' => 'challenges#update'
    
-   # get 'challenges/:id/songs/song_results/show' => 'song'
+  # get 'challenges/:id/songs/song_results/show' => 'song'
 
-   # match "/auth/:provider/callback" => "sessions#create", via: [:get,:post]
-   # match "/signout" => "sessions#destroy", :as => :signout, via: [:get,:post]
+  # match "/auth/:provider/callback" => "sessions#create", via: [:get,:post]
+  # match "/signout" => "sessions#destroy", :as => :signout, via: [:get,:post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
